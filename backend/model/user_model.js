75d0a0c1693800eb3c0efe2eb32f8d32d5fd3bad
arin-prashar <prashararin@gmail.com>
2024-03-22 21:38:37 +0530

    Basic user schema and controller

---
+++ backend/model/user_model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const UserSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+    }
+},{
+    timestamps:true
+});
