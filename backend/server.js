c82a6df4da8f715cf72ef47c969a97d50f0fbc61
arin-prashar <prashararin@gmail.com>
2024-03-23 02:16:10 +0530

    Update dependencies and add login and registration functionality

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 const express = require ('express')
+const process = require('process');
 const bodyParser=require('body-parser')
 const mongoose=require("mongoose")
 const userRoute=require('./routes/user')
 const app=express();
 
-//const db=mongoose.connect("mongodb+srv://nandanupadhyay1234:pkPPznLjgogmQZkC@cluster0.5ltnqvy.mongodb.net/Financemanagement")
-
 const connectDatabase=()=>{
-    mongoose.connect("mongodb+srv://nandanupadhyay1234:pkPPznLjgogmQZkC@cluster0.5ltnqvy.mongodb.net/Financemanagement",{dbName:"Financemanagement"
+    mongoose.connect("mongodb+srv://nandanupadhyay1234:pkPPznLjgogmQZkC@cluster0.5ltnqvy.mongodb.net/Financemanagement",  {dbName:"Financemanagement"
        }).then((data)=>{
             console.log(`Mongodb connected with server: ${data.connection.host}`);
         })    
 } 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/users',userRoute);
@@ -21,10 +19,8 @@ app.get('/', (req, res) => {
   res.send("What's up doc ?!");
 });
 
-connectDatabase();
+connectDatabase(); 
 
-// start the server
 app.listen(3000, () => {
-  console.log(`server running : http://localhost:3000`);
-
-  });
+  console.log('Server is running on port 3000\nServer: http://localhost:3000/');
+});
