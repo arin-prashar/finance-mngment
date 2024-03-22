99e6efc0eb09d110551ff1e16fcdee1de5f23625
Chandan Upadhyay <menotnandan6969@gmail.com>
2024-03-23 02:14:41 +0530

    comit

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,19 @@ const express=require('express');
 const registerUser=require('../controller/userRegister');
 const authentication=require('../middleware/authentication')
 const createBudget  = require('../controller/budget');
+const expenseTracker=require('../controller/expense_tracker')
 
 const router=express.Router();
 
 router.post('/registerUser',registerUser.register);
+
+//Budget Tools
 router.post('/createBudget',authentication.authorization,createBudget.create_budget)
+
+//expense Tracker
+router.post('/createExpense',expenseTracker.create);
+router.get('/displayExpenses',expenseTracker.findAll);
+router.delete('deleteExpense',expenseTracker.delete);
+router.put('/updateExpense',expenseTracker.edit);
 
 module.exports = router;
