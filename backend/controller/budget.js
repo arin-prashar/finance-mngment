bf6158239ba02a9003ee62fd4ec2d22fd56b0b99
arin-prashar <prashararin@gmail.com>
2024-03-22 21:29:12 +0530

    Budget Management First Commit

---
+++ backend/controller/budget.js
@@ -0,0 +1,76 @@
+// budget management
+// student can set monthly budgets for each category
+
+const Budget = require('../models/budget_budget');
+
+
+// Create a new budget catergory
+exports.create_category = async (req, res) => {
+  try {
+    const budget = new Budget({
+      categories: req.body.categories
+    });
+    await budget.save();
+    res.status(201).send(budget);
+  } catch (error) {
+    res.status(400).send(error);
+}};
+
+// Get user's budget using username
+exports.getBudget = async (req, res) => {
+  try {
+    const budget = await Budget.findOne({ username: req.params.username });
+    if (!budget) {
+      return res.status(404).send({ error: 'User/Budget Not Found' });
+    }
+    res.send(budget);
+  } catch (error) {
+    res.status(500).send(error);
+}};
+
+// Update user's budget using username
+exports.updateBudget = async (req, res) => {
+  try {
+    const budget = await Budget.findOne({ username: req.params.username });
+    if (!budget) {
+      return res.status(404).send({ error: 'User/Budget Not Found' });
+    }
+    budget.name = req.body.name;
+    budget.income = req.body.income;
+    budget.categories = req.body.categories;
+    await budget.save();
+    res.send(budget);
+  } catch (error) {
+    res.status(400).send(error);
+}};
+
+// Delete a budget category using username
+expors.deleteCategory = async (req, res) => {
+    try{
+        const budget = await Budget.findOne({ username: req.params.username });
+        if (!budget) {
+            return res.status(404).send({ error: 'User/Budget Not Found' });
+        }
+        budget.categories = budget.categories.filter(category => category.name !== req.params.categoryName);
+        await budget.save();
+        res.send(budget);
+    }
+    catch (error) {
+        res.status(400).send(error);
+}};
+
+exports.new_user = async (req, res) => {
+    try{
+        const budget = new Budget({
+            username: req.body.username,
+            name: req.body.name,
+            income: req.body.income,
+            categories: "None"
+        });
+        await budget.save();
+        res.status(201).send(budget);
+    }
+    catch (error) {
+        res.status(400).send(error);
+}};
+
---
+++ backend/model/budget_model.js
@@ -0,0 +1,48 @@
+// budget model 
+
+const mongoose = require('mongoose');
+
+const budgetSchema = new mongoose.Schema({
+  user: {  // Reference the owner of the budget
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', 
+    required: true 
+  },
+  username:{
+    type: String,
+    required: true
+  },
+  name: {  // Budget name (e.g., "Monthly Budget", "Vacation Fund")
+    type: String,
+    required: true,
+    trim: true
+  },
+  period: { // Budgeting period (e.g., "monthly", "weekly")
+    type: String,
+    enum: 'monthly',
+    default: 'monthly'
+  },
+  startDate: { // Optionally track the start date
+    type: Date
+    //  first of every month
+  },
+  income: { 
+    type: Number,
+    required: true,
+    default: 0
+  },
+  categories: [{ // Array of expense categories 
+    name: {
+        type: String,
+        required: true
+    },
+    limit: {
+        type: Number,
+        required: true
+    }
+  }]
+}, {
+  timestamps: true, // Automatically add 'createdAt' and 'updatedAt' fields
+});
+
+module.exports = mongoose.model('Budget', budgetSchema); 
